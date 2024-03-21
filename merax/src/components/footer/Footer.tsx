f202ad10ceb3607a0cdbe7426b4750fbd7ff5357
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-22 04:43:11 +0500

    add

---
+++ merax/src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Footer({}: Props) {
+  return (
+    <div className='Footer'>
+      <h1>Footer</h1>
+    </div>
+  )
+}
---
+++ merax/src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../../assets/icons/logo.svg'
+import phoneIcon from '../../assets/icons/phoneIcon.svg'
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <div className='Header'>
+      <div className="">
+        <img src={logo} alt="" />
+      </div>
+      <div className="">
+        <p></p>
+        <img src={phoneIcon} alt="" />
+        <button>Bepul konsultatsiya</button>
+      </div>
+    </div>
+  )
+}
