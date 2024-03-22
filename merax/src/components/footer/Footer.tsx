20d1fde05e46d7d06584b159f7abb1aeb82c9ddd
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-22 17:59:16 +0500

    add

--- merax/src/components/footer/Footer.tsx
+++ merax/src/components/footer/Footer.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
-type Props = {}
+import React, { useState } from "react";
+import logo from "../../assets/icons/logo.svg";
+import Image from "next/image";
+type Props = {};
 
 export default function Footer({}: Props) {
+
+  type Ticon = {
+    image:any,
+    url:string,
+  }
+
+  const [footerIcons, setFooterIcons] = useState([
+    {
+      image:logo,
+      url:""
+    }
+  ])
+
   return (
-    <div className='Footer'>
-      <h1>Footer</h1>
+    <div className="Footer mainFont w-full flex justify-center items-center">
+      <div className="w-[1440px] flex flex-col justify-center items-start">
+        <div className="">
+          <Image src={logo} alt="" />
+          <div className="">
+
+          </div>
+        </div>
+        <div className=""></div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ merax/src/components/main/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Banner({}: Props) {
+  return (
+    <div className='Banner '>
+
+    </div>
+  )
+}
