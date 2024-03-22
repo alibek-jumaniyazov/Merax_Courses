cfe13d3297c7814aeeaaf92b8b51f1827a228b5b
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-22 17:13:59 +0500

    add

--- merax/src/components/header/Header.tsx
+++ merax/src/components/header/Header.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import logo from '../../assets/icons/logo.svg'
-import phoneIcon from '../../assets/icons/phoneIcon.svg'
-import Image from 'next/image'
+import React from "react";
+import logo from "../../assets/icons/logo.svg";
+import phoneIcon from "../../assets/icons/phoneIcon.svg";
+import Image from "next/image";
 
-type Props = {}
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <div className='Header'>
-      <div className="">
-        <Image src={logo} alt=""/>
-      </div>
-      <div className="">
-        <p className=''>+998(90) 429-90-19</p>
-        <Image src={phoneIcon} alt=""/>
-        <button>Bepul konsultatsiya</button>
+    <div className="Header mainFont w-full flex justify-center items-center">
+      <div className="w-[1440px] flex justify-between items-center">
+        <div className="">
+          <Image src={logo} alt="" />
+        </div>
+        <p className="">+998(90) 429-90-19</p>
+        <Image src={phoneIcon} alt="" />
+        <div className="flex justify-center items-center gap-">
+          <button className="">Bepul konsultatsiya</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
