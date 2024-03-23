4d3f8cac879c6d5edd3e441430c34c92550dfba4
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-24 03:00:42 +0500

    add

--- merax/src/components/frontend/WhoCourse.tsx
+++ merax/src/components/frontend/WhoCourse.tsx
@@ -8,17 +8,18 @@ type Props = {
 
 export default function WhoCourse({ item }: Props) {
   return (
-    <div className="WhoCourse">
-      <div className="">
-        <Image src={item.image} alt="" />
-      </div>
-      <div className="">
-        <p>{item.title}</p>
-        <div className="">
-          <span>{item.text}</span>
-          <span>{item.info}</span>
+    <div className="WhoCourse w-full flex justify-between items-start  mt-36 ">
+         <div className="w-1/2 flex flex-col justify-center items-start gap-11">
+        <p className="bg-mainColor text-white font-bold text-4xl p-3 rounded-md">{item.title}</p>
+        <div className="flex flex-col justify-center items-start gap-2">
+          <span className="font-semibold text-2xl text-textColor mb-2">{item.text}</span>
+          <span className="w-[90%] font-medium text-textColor text-xl">{item.info}</span>
         </div>
       </div>
+      <div className="w-[40%]">
+        <Image src={item.image} alt="" className="w-full rounded-2xl"/>
+      </div>
+     
     </div>
   );
 }
